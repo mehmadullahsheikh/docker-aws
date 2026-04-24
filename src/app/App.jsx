d85d96e5adcb5038254400a30f5d5491abf096c3
mehmadullahsheikh <mehmadullahsheikh@gmail.com>
2026-04-24 19:37:54 +0530

    added

---
+++ src/app/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css"
+function App() {
+  return (
+    <>
+    <main className="h-screen w-full bg-gray-950 flex gap-4 p-4">
+      <aside className="h-screen w-1/4 rounded-lg bg-amber-100"></aside>
+       <section className="h-screen w-3/4 bg-neutral-300 rounded-lg"></section>
+    </main>
+    </>
+  )
+}
+
+export default App
